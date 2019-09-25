c23d50039df132adf1a75d1e531fb2241df3801d
snod4 <csnodgrass233@gmail.com>
2019-09-24 21:10:43 -0500

    HW 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get "/", to: "homepage#home", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  
 end
