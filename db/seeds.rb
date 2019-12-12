bc3245682e66b37b16dbf597f664a1c37720f771
snod4 <csnodgrass233@gmail.com>
2019-12-11 23:07:26 -0600

    HW13

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ProjectInfo.create(name: "Java Paint", description: "This is a test, but the project is real and old", date: "Summer 2016")
+ProjectInfo.create(name: "Java Paint", description: "A Microsoft Paint-esque application that was built from the ground up utilizing Java's built-in GUI library. Drawing, various line sizes, text-boxes, snipping, movable menus, color-selectors, and opening/saving projects were all implmented.", date: "Summer 2016")
 
 
 ProjectInfo.create(name:"Password Cracker", description:"Utilizes GPU processing in order to compare provided MD5 hashes against GPU-generated hashes. We were able to crack 1,000,000 alpabetic passwords with limited length in roughly 4 minutes.",  date: "Spring 2019")
